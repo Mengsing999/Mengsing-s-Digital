2bbd791320970701fc8458364830ffc16fc7b921
Mengsing's Digital <mengsing2025@gmail.com>
2025-04-28 12:55:23 +0700

    Create App.jsx

---
+++ textarea-app/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div style={{ marginTop: "20px", textAlign: "center" }}>
+      <textarea
+        placeholder="พิมพ์ข้อความของคุณ..."
+        style={{
+          width: "80%",
+          height: "100px",
+          fontSize: "16px",
+        }}
+      />
+    </div>
+  );
+}
+
+export default App;
